af82e724d36a420654250b6bbdaea2e5add07c39
bogdanov <iceposhta@gmail.com>
2021-06-01 19:22:25 +0100

    Initial Commit

---
+++ Guess My Number/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess My Number
+//
+//  Created by bogdanov on 01.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess My Number/Guess_My_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_My_NumberApp.swift
+//  Guess My Number
+//
+//  Created by bogdanov on 01.06.21.
+//
+
+import SwiftUI
+
+@main
+struct Guess_My_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
