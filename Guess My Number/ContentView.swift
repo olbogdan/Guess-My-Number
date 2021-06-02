f761b658868215f17679654fa2e12ac17c372bd3
bogdanov <iceposhta@gmail.com>
2021-06-02 22:26:46 +0100

    implemented: basic user interface

--- Guess My Number/ContentView.swift
+++ Guess My Number/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderPosition: Double = 50
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("Point 🎯 as close as you can to")
+                    .padding()
+                Text("89")
+            }
+            HStack {
+                Text("1")
+                Slider(value: $sliderPosition, in: 1 ... 100)
+                Text("100")
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
