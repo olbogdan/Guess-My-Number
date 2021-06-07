11c5b9a7419339a8982690cfb02db9b70e0cf15f
bogdanov <iceposhta@gmail.com>
2021-06-07 23:58:48 +0200

    add paddings & colors to a game view

--- GuessMyNumber/Views/ContentView.swift
+++ GuessMyNumber/Views/ContentView.swift
@@ -13,40 +13,57 @@ struct ContentView: View {
     @State private var game = Game()
 
     var body: some View {
-        VStack {
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
             VStack {
-                Text("Point 🎯 as close as you can to")
-                    .kerning(2.0)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(4.0)
-                    .font(.footnote)
-                Text(String(game.target))
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .lineSpacing(-1.0)
+                VStack {
+                    Text("Point 🎯 as close as you can to")
+                        .kerning(2.0)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .lineSpacing(4.0)
+                        .font(.footnote)
+                    Text(String(game.target))
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .lineSpacing(-1.0)
+                }
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: $sliderPosition, in: 1 ... 100)
+                    Text("100")
+                        .bold()
+                }
+                Button(action: {
+                    alertIsVisible = true
+                }) {
+                    Text("Hit me")
+                        .bold()
+                        .font(.title3)
+                        .textCase(.uppercase)
+                }
+                .padding()
+                .background(
+                    ZStack {
+                        Color("ButtonColor")
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                    }
+                )
+                .foregroundColor(.white)
+                .cornerRadius(16.0)
+                .alert(isPresented: $alertIsVisible) {
+                    let roundedValue = Int(sliderPosition.rounded())
+                    return Alert(title: Text("Result"),
+                                 message: Text(
+                                     "The slider's value is \(roundedValue)\n"
+                                         + "You scored \(game.points(sliderValue: roundedValue)) points this round."
+                                 ),
+                                 dismissButton: .default(Text("great!")))
+                }
             }
-            HStack {
-                Text("1")
-                    .bold()
-                Slider(value: $sliderPosition, in: 1 ... 100)
-                Text("100")
-                    .bold()
-            }
-            Button(action: {
-                alertIsVisible = true
-            }) {
-                Text("Hit me")
-            }
-            .alert(isPresented: $alertIsVisible) {
-                let roundedValue = Int(sliderPosition.rounded())
-                return Alert(title: Text("Result"),
-                      message: Text(
-                        "The slider's value is \(roundedValue)\n"
-                            + "You scored \(game.points(sliderValue: roundedValue)) points this round."
-                      ),
-                      dismissButton: .default(Text("great!")))
-            }
+            .padding()
         }
     }
 }
