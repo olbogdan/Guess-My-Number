395e8626ef22f9e58d7f207e3b1d89f2cd31f34a
bogdanov <iceposhta@gmail.com>
2021-06-10 01:51:26 +0200

    implemented: LabelText, RoundedRectTextView

--- GuessMyNumber/Views/TextViews.swift
+++ GuessMyNumber/Views/TextViews.swift
@@ -41,6 +41,34 @@ struct SliderLabel: View {
     }
 }
 
+struct LabelText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .kerning(1.5)
+            .foregroundColor(Color("TextColor"))
+            .font(.caption)
+            .bold()
+            .textCase(.uppercase)
+    }
+}
+
+struct RoundedRectTextView: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .font(.title)
+            .frame(minWidth: 68, minHeight: 55)
+            .foregroundColor(Color("TextColor"))
+            .background(
+                RoundedRectangle(cornerRadius: 21)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(Color("ButtonStrokeColor"))
+            )
+    }
+}
 
 struct TextViews_Previews: PreviewProvider {
     static var previews: some View {
@@ -48,6 +76,17 @@ struct TextViews_Previews: PreviewProvider {
             InstructionText(text: "title")
             BigText(text: "999")
             SliderLabel(text: "11")
-        }
+            LabelText(text: "text")
+            RoundedRectTextView(text: "999")
+        }.previewLayout(.fixed(width: 200, height: 200))
+
+        VStack {
+            InstructionText(text: "title")
+            BigText(text: "999")
+            SliderLabel(text: "11")
+            LabelText(text: "text")
+            RoundedRectTextView(text: "999")
+        }.previewLayout(.fixed(width: 200, height: 200))
+            .preferredColorScheme(.dark)
     }
 }
